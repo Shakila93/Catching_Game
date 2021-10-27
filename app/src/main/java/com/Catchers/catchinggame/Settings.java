f3225241c145f9d982f64e6aa882a7fc48c560f7
shakila <shakilamaqsoodi@outlook.com>
2021-10-27 17:38:42 +1300

    Initial commit 2

--- app/src/main/java/com/Catchers/catchinggame/Settings.java
+++ app/src/main/java/com/Catchers/catchinggame/Settings.java
@@ -3,12 +3,20 @@ package com.Catchers.catchinggame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Settings extends AppCompatActivity {
-
+    private Button ChangeBG;
+    private Button ReturnToMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        ChangeBG = findViewById(R.id.ChangeView);
+        ReturnToMain = findViewById(R.id.ReturnButton);
+
+        ReturnToMain.setOnClickListener(view -> finish());
     }
+
 }
